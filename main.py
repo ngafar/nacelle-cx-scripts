2026c433dae1cbc4733a4626b68c58cc5c6e674c
Nawaz Gafar <gafar.nawaz@gmail.com>
2022-10-13 11:23:10 -0400

    Added product/collection selector

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import csv
+import sys
 import json
 import hashlib
 import nacelle 
 from tqdm import tqdm
+
+selected_option = input('Scan products or collections (P/C): ')
+selected_option = selected_option.lower()
+
+if (selected_option != 'p') and (selected_option != 'c'):
+    print("Invalid selection")
+    sys.exit()
 
 # --------------------------------------
 # Get all PRODUCTS in a space
@@ -19,8 +27,11 @@ with open('./data/spaces.csv', 'r') as spaces_file:
         space_id = row[0]
         token = row[1]
         
-        prods = nacelle.get_all_products(space_id, token)
-        
+        if selected_option == 'p':
+            prods = nacelle.get_all_products(space_id, token)
+        else:
+            prods = nacelle.get_all_collections(space_id, token)
+    
         for p in prods:
             prods_list.append(p)
 
@@ -33,13 +44,19 @@ print('Step 2: Getting API response for each product')
 n = 0
 for row in tqdm(prods_list):
     # nacelle entry id
-    entry_id_result = nacelle.get_prod_by_id(space_id=row[0], token=row[1], entry_id=row[2])
+    if selected_option == 'p':
+        entry_id_result = nacelle.get_prod_by_id(space_id=row[0], token=row[1], entry_id=row[2])
+    else:
+        entry_id_result = nacelle.get_collection_by_id(space_id=row[0], token=row[1], entry_id=row[2])
 
     entry_id_json = json.dumps(entry_id_result)
     entry_id_hash = hashlib.md5(entry_id_json.encode("utf-8")).hexdigest()
 
     # handle
-    handle_result = nacelle.get_prod_by_handle(space_id=row[0], token=row[1], handle=row[3])
+    if selected_option == 'p':
+        handle_result = nacelle.get_prod_by_handle(space_id=row[0], token=row[1], handle=row[3])
+    else:
+        handle_result = nacelle.get_collection_by_handle(space_id=row[0], token=row[1], handle=row[3])
 
     handle_json = json.dumps(handle_result)
     handle_hash = hashlib.md5(handle_json.encode("utf-8")).hexdigest()
