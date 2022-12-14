1b3485e17ed01512b26b24b95703cccef5d6afa3
Nawaz Gafar <gafar.nawaz@gmail.com>
2022-12-14 16:18:03 -0500

    Added check to collection-prod-order

--- collection-prod-order/main.py
+++ collection-prod-order/main.py
@@ -1,5 +1,19 @@
+import os
 import shopify
 import nacelle
+from dotenv import load_dotenv
 
-shopify_list = shopify.get_collection_prods()
-nacelle_list = nacelle.get_collection_prods(space_id, token, collection_handle)
+load_dotenv() 
+
+SHOPIFY_DOMAIN = os.getenv("SHOPIFY_DOMAIN")
+SHOPIFY_TOKEN = os.getenv("SHOPIFY_TOKEN")
+NACELLE_SPACE_ID = os.getenv("NACELLE_SPACE_ID")
+NACELLE_TOKEN = os.getenv("NACELLE_TOKEN")
+
+collection_handle = 'shorts'
+
+shopify_prod_list = shopify.get_collection_prods(SHOPIFY_DOMAIN, SHOPIFY_TOKEN, collection_handle)
+nacelle_prod_list = nacelle.get_collection_prods(NACELLE_SPACE_ID, NACELLE_TOKEN, collection_handle)
+
+match = shopify_prod_list == nacelle_prod_list
+print(match)
